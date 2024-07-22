38828b13d1744403128627ac8b402b8ba0bed4d0
QuemeraT <adrianortizzt1vl@gmail.com>
2024-07-22 14:30:40 -0300

    Actualización del controller del sign up

--- back/src/controllers/signUpController.js
+++ back/src/controllers/signUpController.js
@@ -6,14 +6,23 @@ const postUserController = async (userData) => {
       userData;
 
     if (password === rPassword) {
-      const user = await User.create({
-        email,
-        password,
-        username,
-        image,
-        description,
+      const [user, created] = await User.findOrCreate({
+        where: {
+          email,
+        },
+        defaults: {
+          password,
+          username,
+          image,
+          description,
+        },
       });
-      return user;
+
+      if (created) {
+        return user;
+      } else {
+        return "El email ya está asociado a una cuenta";
+      }
     } else {
       return "Las contraseñas no coinciden";
     }
